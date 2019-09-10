e499e3709583b44f7d6fc6d346b3c845ad756c5f
DiogoAbu <diogodeazevedosilva@gmail.com>
2019-09-10 20:35:09 -0300

    Move mobile and services to packages folder. Add husky and lint-staged. Fix ESlint and Prettier.

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  arrowParens: 'always',
-  endOfLine: 'lf',
-  jsxBracketSameLine: false,
-  jsxSingleQuote: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'all',
-};
